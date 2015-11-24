326c41f3b0a8749310385327ba219ed334b64eb3
MAYANK <mayankanand108@gmail.com>
2015-11-23 17:54:03 -0800

    moving the generator to terminal condition of the recursion function

--- inference.py
+++ inference.py
@@ -27,28 +27,41 @@ class kb(object):
     def infer(self, quest, conjunctUnificationRes ={}):
         
         
+        print "this is quest and conjunctUnificationRes", quest, conjunctUnificationRes
+        
         #lhsRegex = re.compile('(\w+)(.*)')
         m = re.match(self.regex, quest)
-        
-        print "this is quest", quest
-        print m
-        
         predicate = m.group(1)
         predArgs = m.group(2)
         
+        print '************** predargs'
         print predArgs
         
         constant = predArgs.split(",") #[x.strip for x in predArgs.split(",")]
         
         #looking for the predicate in the facts to see if it is available        
         if predicate in self.facts:
-            for fact in self.facts[predicate]:
-                print 'printing facts', fact
-                if predArgs in fact:
-                    print "True"
-                    return "True"
+            #checking whether unification is complete
+            #this check needs to be fixed right now
+            if self.isVariableInQuestArgs(constant): 
+                #need to ensure that the arguments matches the finc signature
+                for choice in self.generateSub(constant, predicate):
+                    print "printing the generate stuff", choice
+                    conjunctUnificationRes.update(choice)
+                    print "returning true afte unifictaion and the theta valus is", conjunctUnificationRes
+                    return True
+                
+                
+            else:
+                for fact in self.facts[predicate]:
+                    print 'printing facts', fact
+                    if predArgs in fact:
+                        print "returning true after finding the quest in facts"
+                        return True
+                    else:
+                        return False
         
-        #checking if predicate is a  part of the clause
+        #checking if predicate is a part of the clause
         if not predicate in self.clauses:
             print "Cant be proved as there are no predcate in clauses"
             return "False"
@@ -81,102 +94,77 @@ class kb(object):
                 for clause in conjunctiveClauses:
                     
                     print "clause ----------->", clause 
+                    unifiedClause = self.replaceWithTheta(clause, conjunctUnificationRes)
+                    print "the unifiedClause and the status of theta now"
+                    print unifiedClause, conjunctUnificationRes
                     
-                    #check if there are exra variables in the lhs of the implication when comapred to rhs
-                    #if found then add them to theta so that it can be used by the generator
-                    
-                    m = re.match(self.regex, clause)
-                    lhsPredicate = m.group(1)
-                    lhsPredArgs = m.group(2)
-                    lhsArgs = lhsPredArgs.split(",")
-                    
-                    for newArg in lhsArgs:      #adds new variable to the deep copy of the theta mapping
-                        if not newArg in conjunctUnificationRes:
-                            conjunctUnificationRes[newArg] = newArg
-                    
-                    
-                    
-                    print "check if partial unification is done and theta right now is", conjunctUnificationRes
-                    print self.isVariableInTheta(conjunctUnificationRes)
-                    #checking whether unification is complete
-                    if self.isVariableInTheta(conjunctUnificationRes):
-                        if lhsPredicate in self.facts:     #checking if the generator can be used
-                            partiallyUnifiedArgList =  []        #unified list
-                            for word in lhsArgs:
-                                partiallyUnifiedArgList.append(conjunctUnificationRes.get(word,''))
-                            
-                            print "printing unified argumenst list"
-                            print partiallyUnifiedArgList
-                    
-                            #need to ensure that the arguments matches the finc signature
-                            for choice in self.generateSub(partiallyUnifiedArgList, lhsPredicate):
-                                conjunctUnificationRes.update(choice)
-                            
-                                unifiedClause = self.replaceWithTheta(clause, conjunctUnificationRes)
-                        
-                                print 'unified clause ****************'
-                                print unifiedClause 
-                                if not self.infer(unifiedClause, conjunctUnificationRes):
-                                    break
-                                
-                                
-
-                    else:
-                        unifiedClause = self.replaceWithTheta(clause, conjunctUnificationRes)
-                        if not self.infer(unifiedClause, conjunctUnificationRes):
-                            break            #     break 
-                                
+                    if not self.infer(unifiedClause, conjunctUnificationRes):
+                        print "<<<<<<<<<<<<<<<<<<<<breaking here because the inference returned is false"
+                        break
+                                 
             return True
         return False
     
-    
-    
-    
-    
-    
-# print '**************'
-# print self.visitedClauses
-# 
-# if unifiedClause not in self.visitedClauses:
-#     self.visitedClauses.append(unifiedClause)
-# else:
-#     print "loop found"
-#     self.visitedClauses = []
-
-    
-    
     def generateSub(self, queryArgsList, predicate):
+        """
+            Generator used to generate different values of theta whenever possible
+            @param: queryArgsList -> the arguments of the query which has been unified so far
+                    predicate -> predicate of the query
+            returns a new theta dict everytime
+        """
         
         factArgList = self.facts[predicate]
+        
         for eachArgSet in factArgList:
             thetaDict = {}
             counter = 0
             removeParanRegex = re.compile('\((.*?)\)')
             m = re.search(removeParanRegex, eachArgSet)
             eachArgSet = m.group(1)
-            factArgList = eachArgSet.split(',')
+            eachArgList = eachArgSet.split(',')
+            
+            # print "------------------ > each queryarglist and eacharglist and eachargset"
+            # print queryArgsList, eachArgList, eachArgSet
             
             for arg in queryArgsList:
                 if self.isVariable(arg):
-                    thetaDict[arg]= factArgList[counter]
+                    thetaDict[arg]= eachArgList[counter]
                 else:
-                    if arg == factArgList[counter]:
+                    # print '^^^^^^^^^^^^^^^^^^^^^^^^'
+                    # print arg, eachArgList[counter]
+                    
+                    if arg == eachArgList[counter]:
                         pass
                     else:
                         yield {}
     
                 counter += 1
+            # print '&&&&&&&&&&&& this has to be fixed'
+            # print thetaDict
             yield thetaDict
     
     
-    def isVariableInTheta(self, theta):
-        for k, v in theta.iteritems():
-            if self.isVariable(v):
+    def isVariableInQuestArgs(self, questArgs):
+        """
+            @param: questArgs is a list which contains the arguments of quest
+            helper fucntion to check if genrator is needed
+            returns True if variable found else return False
+        """
+        
+        for arg in questArgs:
+            if self.isVariable(arg):
                 return True
         return False
     
     
     def replaceWithTheta(self, expr, thetaDict):
+        
+        """
+            helper function to replace the expression with unified expression
+            @param: expr is a string
+                    thetaDict is a dict
+            returns the subtituted expression
+        """
         
         for variable, constant in thetaDict.iteritems():
             if variable in expr:    #this should be refined as it is very crude right now
@@ -185,6 +173,13 @@ class kb(object):
     
     
     def isVariable(self, string):
+        
+        """
+            @param: string
+            helper function to check if the token is a variable or not.
+            return True if it is a variable else returns False
+        """
+        
         regex = re.compile("^[a-z]")
         m = re.match(regex, string)
         
@@ -196,23 +191,34 @@ class kb(object):
             return False
     
     
-    def unify(self, s1List, s2List, unifierDict = {}):
+    def unify(self, argumentList, constantList, unifierDict = {}):
         #need to standerdize here before unification      
+        """
+            function to unify arguments of 2 instances of a predicate
+            with each other
+            @param: argumentList -> list containing arguments with variables instance
+                    constantList -> list containing arguments with constants and variables
+                    unifierDict is empty by default
+            returns the uninfied dict if unification was possible
+                    else returns False
+        """
+        
+        
         counter = 0
         
-        for item in s1List:
-            if (self.isVariable(item) and self.isVariable(s2List[counter])):
-                unifierDict[item] = s2List[counter] #replace one variable by another
-            elif (not self.isVariable(item) and self.isVariable(s2List[counter])):
-                unifierDict[s2List[counter]] = item
-            elif (self.isVariable(item) and  not self.isVariable(s2List[counter])):
-                unifierDict[item] = s2List[counter]
+        for item in argumentList:
+            if (self.isVariable(item) and self.isVariable(constantList[counter])):
+                unifierDict[item] = item #replace one variable by another
+            elif (not self.isVariable(item) and self.isVariable(constantList[counter])):
+                unifierDict[constantList[counter]] = item
+            elif (self.isVariable(item) and  not self.isVariable(constantList[counter])):
+                unifierDict[item] = constantList[counter]
             else:   #in this case both are constants and hence cant be unified
                 print "false and cant be unified"
                 return False
             counter += 1
-        print "True inside the unifier"
-        print unifierDict
+        # print "True inside the unifier"
+        # print unifierDict
         return unifierDict
     
 if __name__ == "__main__":
